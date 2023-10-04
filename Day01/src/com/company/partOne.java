caa4493e92fb62ebfe871ecd9516824f14c77dd0
Ben <benarcher71@gmail.com>
2023-10-04 15:25:59 -0400

    adding dayFour

---
+++ Day01/src/com/company/partOne.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+
+public class partOne {
+
+    public static void main(String[] args) throws FileNotFoundException{
+	    File file = new File("data.txt");
+        Scanner scanner = new Scanner(file);
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        Integer currTotal = 0;
+
+        while (scanner.hasNextLine()) {
+            String curr = scanner.nextLine();
+            if (curr.equals("")) {
+                maxHeap.add(currTotal);
+                currTotal = 0;
+            } else {
+                currTotal += Integer.parseInt(curr);
+            }
+        }
+
+        Integer topSum = 0;
+        topSum += maxHeap.remove();
+        System.out.println(topSum);
+
+        scanner.close();
+    }
+}
